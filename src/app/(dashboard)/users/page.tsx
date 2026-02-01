0eccb0fbfa90d58162bf0bf72fb815be6c7eb131
Idris Agboola <agboolaisholaidreez@gmail.com>
2026-02-01 15:02:59 +0100

    update

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -14,8 +14,7 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { Button } from "@ui/Button";
-import { Table } from "@ui/Table/Table";
-import { TableFooter } from "@ui/Table/TableFooter";
+import { Table, TableFooter } from "@ui/Table/";
 import { TextField } from "@ui/TextField";
 import { Typography } from "@ui/Typography";
 import clsx from "clsx";
